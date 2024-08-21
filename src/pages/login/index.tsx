5ac9fd671d097b89a242d1b4755239d08e61ed2a
vstnt <r.rms087@gmail.com>
2024-08-21 13:57:34 -0300

    Página Login tornada mobile friendly

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -80,65 +80,64 @@ export default function Login() {
 
   return (
     <>
-      <div id='bg' className={` min-h-[550px] min-w-[600px] pt-48 pb-40 w-full h-full flex items-center justify-center bg-gradient-to-r 
+      <div id='bg' className={`pt-28 md:min-h-[550px] md:min-w-[600px] md:pt-32 pb-14 w-full h-full flex items-center justify-center bg-gradient-to-r 
       ${theme === 'dark' ? 'from-violet-500 via-violet-900 via-25% to-violet-400 to-95%' 
       : 'from-emerald-200 via-gray-100 via-[3%] to-white to-100% text-stone-900'}  `}> 
-        <div id='conteiner inteiro' 
-        className={` min-h-[550px] min-w-[560px] w-8/12 h-96 flex items-center justify-center rounded-lg overflow-hidden 
+        
+        <div id='conteiner completo, bg-img + formulário' 
+        className={` w-10/12 flex flex-col justify-center rounded-lg overflow-hidden md:w-8/12 md:h-96 md:min-h-[550px] md:min-w-[560px] md:flex-row 
         ${theme === 'dark' ? '' : 'border-2 border-gray-400'} `}>
           
-          <div id='bg img do conteiner' 
-          style={{ backgroundImage: `url('/assets/bglogin.jpg')` }} 
-          className="bg-purple-600 w-2/4 h-full relative bg-cover">
+          <div id='bg-img' 
+          style={{ backgroundImage: `url('/assets/bglogin.jpg')`}} 
+          className="bg-purple-600 md:w-2/4 w-full h-40 md:h-full bg-cover bg-bottom">
           </div>
 
-          <div id='área do formulário' className="min-w-[250px] w-1/2 h-full bg-purple-800 flex flex-col items-center justify-center">
-            <div className="flex flex-col justify-center bg-violet-200 px-4 w-full rounded-md h-full">
-              
-              <div id='img e texto' className="mb-10 font-semibold text-3xl p-2 text-center w-full text-slate-800">
-                <div className="w-full flex items-center justify-center ">
-                  <img
-                    className={` w-[60px] mb-2 rounded-full 
-                      ${theme === 'dark' ? 'bg-emerald-300 border-slate-200' : 'bg-emerald-100 border-slate-400'}  p-1 border-4  `}
-                    src="/assets/login.png"
-                  />
-                </div>
-                <div>Log in</div>
-                <div className="mt-1 text-sm text-slate-600">
-                  Entre na sua conta
-                </div>
-              </div>
-              
-              <div id='formulário'
-              className="-mt-5 flex flex-col items-center justify-center gap-4">
-                <input
-                  placeholder="Email"
-                  className="placeholder-gray-500 shadow-slate-500 text-sm bg-white border-1 rounded-full w-7/12 pl-4 p-2 shadow-md mx-auto text-black"
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
+          <div title="área formulário" className="flex flex-col items-center justify-center bg-violet-200 pt-3 md:pt-0  w-full md:w-1/2 rounded-md h-full">
+            
+            <div id='img e texto' className="md:mb-10 font-semibold text-3xl p-2 text-center w-full text-slate-800">
+              <div className="w-full flex items-center justify-center ">
+                <img
+                  className={` w-[60px] mb-2 rounded-full 
+                    ${theme === 'dark' ? 'bg-emerald-300 border-slate-200' : 'bg-emerald-100 border-slate-400'}  p-1 border-4  `}
+                  src="/assets/login.png"
                 />
-                <input
-                  placeholder="Senha" 
-                  type="password"
-                  className="placeholder-gray-500 text-sm bg-white border-1 rounded-full w-7/12 pl-4 p-2 shadow-md shadow-slate-500 mx-auto text-black"
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                />
-                <button 
-                  onClick={handleLogin}
-                  className={` mt-3 text-sm w-1/3 p-2 shadow-md rounded-xl
-                  ${theme === 'dark' ? 'bg-indigo-950 text-white' : 'bg-indigo-300 border-2 border-gray-500 text-black'} `}>
-                  Entrar
-                </button>
-                <div className="mt-4 border-t w-8/12 border-slate-400"></div>
-                <div className="text-slate-800 -mt-3 -mb-1">Quero acessar com minhas redes sociais</div>
-                
-                <div id="firebaseui-auth-container"></div>
-                <div id="loader">Loading...</div>
               </div>
-
+              <div>Log in</div>
+              <div className="mt-1 text-sm text-slate-600">
+                Entre na sua conta
+              </div>
             </div>
-          </div>          
+            
+            <div id='formulário'
+            className="md:-mt-5 flex flex-col items-center justify-center gap-4 w-10/12 ">
+              <input
+                placeholder="Email"
+                className="placeholder-gray-500 shadow-slate-500 text-sm bg-white border-1 rounded-full w-full  md:w-8/12  pl-4 p-2 shadow-md mx-auto text-black"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
+              <input
+                placeholder="Senha" 
+                type="password"
+                className="placeholder-gray-500 text-sm bg-white border-1 rounded-full w-full md:w-8/12 pl-4 p-2 shadow-md shadow-slate-500 mx-auto text-black"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+              <button 
+                onClick={handleLogin}
+                className={` mt-3 text-sm w-1/3 p-2 shadow-md rounded-xl
+                ${theme === 'dark' ? 'bg-indigo-950 text-white' : 'bg-indigo-300 border-2 border-gray-500 text-black'} `}>
+                Entrar
+              </button>
+              <div className="mt-2 md:mt-4 border-t w-8/12 border-slate-400"></div>
+              <div className="text-xs text-slate-800 -mt-3 -mb-5 md:-mb-1  ">Quero acessar com minhas redes sociais</div>
+              
+              <div id="firebaseui-auth-container" className="mb-3 md:mb-0"></div>
+              <div id="loader">Loading...</div>
+            </div>
+
+          </div>
         </div>
       </div>
     </>
