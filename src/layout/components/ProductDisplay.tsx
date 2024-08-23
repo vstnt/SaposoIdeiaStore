638a66dfb3e4a0ae1370a3e4bb12948d2f75ec26
vstnt <r.rms087@gmail.com>
2024-08-23 12:24:27 -0300

    correção buscando produtos na página de produtos

--- src/layout/components/ProductDisplay.tsx
+++ src/layout/components/ProductDisplay.tsx
@@ -28,13 +28,31 @@ const ProductDisplay: React.FC<ProductDisplayProps> = ({ productId }) => {
 
 
 
-  if (product?.id==null) {
+  if (product == null) {
     return (
-      <div className=" col-span-1 bg-slate-100/90 border-2 border-lime-400 to-slate-500/80 text-black shadow-md rounded flex h-20 gap-2 pl-2 py-[2px]">
-          <div className="px-1 h-full w-full flex justify-center items-center text-sm  ">
-            <p className="text-md font-bold ">Produto não encontrado.</p>
+      <div id="caixa completa" className={` col-span-1  border-x-2 border-b-2 to-slate-500/80 text-black shadow-md rounded flex h-20 gap-2 pl-2 py-[2px]
+        ${theme === 'dark' ? 'bg-black/30 border-slate-400 text-neutral-200 border-t' 
+        : 'border-slate-300 bg-slate-100/90'}`}>
+          
+          <div id="área img" className=" rounded basis-1/5 flex items-center justify-center"><img id='img produto' className="py-1 object-scale-down max-w-full max-h-full basis-1/4 rounded-md" src='/assets/notFound.jpg'></img></div>
+          
+          <div id="área Nome + Descrição" className="basis-3/5 h-full flex flex-col gap-1 text-sm text-justify pr-3">
+            <p className=" text-md font-bold ">Buscando produto</p>
+            <p className="text-[10px] italic leading-tight overflow-hidden line-clamp-3 md:line-clamp-4">será que esse produto existe mesmo?</p>
           </div>
-      </div>    
+    
+          <div id="área valor + botão ver mais" className="basis-1/5 flex flex-col items-end justify-between text-xs self">
+            <div id="preço" className="mt-2 font-semibold flex w-20">R$ -----</div> 
+            <button id="botão ver mais" 
+            onClick={() => redirect()} 
+              className={` rounded px-1 py-1 shadow-lg hover:underline
+              ${theme === 'dark' ? 'bg-slate-500/50 text-white ' 
+              : ' text-black bg-slate-200'}`}>
+              ver mais
+            </button>
+          </div>
+        
+        </div>  
     );
   }
 
