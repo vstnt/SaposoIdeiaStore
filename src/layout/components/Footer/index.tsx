ad4a332dc843d515979b8fb8f87df5b14c6c8a4d
vstnt <r.rms087@gmail.com>
2024-09-02 16:25:34 -0300

    ajuste no footer

--- src/layout/components/Footer/index.tsx
+++ src/layout/components/Footer/index.tsx
@@ -5,14 +5,14 @@ export default function Footer() {
     const { theme } = useTheme()    
 
     return (
-        <div className={`px-12 grid grid-cols-2 md:grid-cols-4 justify-items-center items-center h-60 border-t absolute bottom-auto w-full
+        <div className={`px-12 grid grid-cols-1 md:grid-cols-4 justify-items-center items-center h-60 border-t absolute bottom-auto w-full
         ${theme === 'dark' ? 'bg-[#0a0b25] border-indigo-500' 
         : 'bg-gradient-to-b from-slate-200 from-65% to-violet-200 text-black border-t-[2px] border-indigo-300'}`}>
             
-            <div className="font-mono text-lg hover:underline"><Link to={'https://wa.me/message/B6TFIBMN7YE2H1'}>Contato dev Ricardo Mass</Link></div>
             <div className="font-mono text-lg">SapoSaposo</div>
             <div className="font-mono text-lg">SapoSaposo</div>
             <div className="font-mono text-lg">SapoSaposo</div>
+            <div className="font-mono text-lg underline underline-offset-4"><Link to={'https://wa.me/message/B6TFIBMN7YE2H1'}>Contato dev Ricardo Mass</Link></div>
 
 
         </div>
