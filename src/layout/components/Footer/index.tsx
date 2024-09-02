ec14f99d910c6c12c6b1eb71ce9641e2ac145ab0
vstnt <r.rms087@gmail.com>
2024-09-02 15:46:22 -0300

    ajuste no footer

--- src/layout/components/Footer/index.tsx
+++ src/layout/components/Footer/index.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     const { theme } = useTheme()    
 
     return (
-        <div className={`px-12 grid grid-cols-2 md:grid-cols-6 justify-items-center items-center h-60 border-t absolute bottom-auto w-full
+        <div className={`px-12 grid grid-cols-2 md:grid-cols-4 justify-items-center items-center h-60 border-t absolute bottom-auto w-full
         ${theme === 'dark' ? 'bg-[#0a0b25] border-indigo-500' 
         : 'bg-gradient-to-b from-slate-200 from-65% to-violet-200 text-black border-t-[2px] border-indigo-300'}`}>
             
@@ -13,14 +13,7 @@ export default function Footer() {
             <div className="font-mono text-lg">SapoSaposo</div>
             <div className="font-mono text-lg">SapoSaposo</div>
             <div className="font-mono text-lg">SapoSaposo</div>
-            <div className="font-mono text-lg">SapoSaposo</div>
-            <div className="font-mono text-lg">SapoSaposo</div>
-            <div className="font-mono text-lg">SapoSaposo</div>
-            <div className="font-mono text-lg">SapoSaposo</div>
-            <div className="font-mono text-lg">SapoSaposo</div>
-            <div className="font-mono text-lg">SapoSaposo</div>
-            <div className="font-mono text-lg">SapoSaposo</div>
-            <div className="font-mono text-lg">SapoSaposo</div>
+
 
         </div>
     )
